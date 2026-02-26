7a64797c9d6ab42baceded3310ceef68702ddac3
sukanto01899 <sukanto01899@gmail.com>
2026-02-26 17:08:46 +0600

    builder code changed

--- lib/builder-code.ts
+++ lib/builder-code.ts
@@ -1,6 +1,6 @@
 import { Attribution } from "ox/erc8021";
 
-export const BUILDER_CODE = "bc_2vrmw1ci";
+export const BUILDER_CODE = "bc_wyuw80t8";
 export const BUILDER_DATA_SUFFIX = Attribution.toDataSuffix({
   codes: [BUILDER_CODE],
 });
